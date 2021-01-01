270373e9be460f7bffe81f2818da9b9b52411128
Emily Ritter <ritter@gcgmarketing.com>
2021-01-01 17:49:26 -0600

    Adds axios npm and makes call to unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID vtTg67S9a43z5YoNevw0grNcvi_9yDodipwROOBKE0g'
+      }
+    })
   }
 
   render() {
