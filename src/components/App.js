14a426ca9a20c11e4d4babaebf982f6d2cc9aecd
Emily Ritter <ritter@gcgmarketing.com>
2021-01-01 16:51:31 -0600

    Removes default src files and adds basic project scaffolding

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Reach from 'react';
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
+const App = () => {
+  return (
+    <div>
+    App
+    </div>
+  )
+}
+
+export default App;
